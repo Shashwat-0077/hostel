6176065666910a0381bc92f407091e794c5f0054
Shashwat-0077 <102245652+Shashwat-0077@users.noreply.github.com>
2023-11-21 18:33:50 +0530

    Update config.php
    ewfgersdbhderth

--- hostel/includes/config.php
+++ hostel/includes/config.php
@@ -1,7 +1,7 @@
 <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
+$dbuser="hostel-management-main-db-0f5c2a99569f58da9";
+$dbpass="VBgATwp9P3PE55Ed3uxW9bNnUtbAR3";
+$host="adaptable-prod.database.windows.net";
 $db="hostel";
 $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
 ?>
